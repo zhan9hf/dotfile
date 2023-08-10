d7f5348961c55b0ba7dc9e55e956256db6058634
Zhang Haifeng <94585823+zhan9hf@users.noreply.github.com>
2023-08-10 13:09:39 +0800

    add windows script

---
+++ profile.ps1
@@ -0,0 +1,2 @@
+oh-my-posh init pwsh | Invoke-Expression
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
---
+++ startup.ps1
@@ -0,0 +1,53 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install 7zip
+
+choco install git
+
+choco install visualstudio2017buildtools
+
+choco install neovim
+
+choco install cmake
+
+choco install powershell-core --pre
+
+choco install llvm
+
+choco install python
+
+choco install ruby
+
+choco install nodejs
+
+choco install strawberryperl
+
+choco install ripgrep
+
+choco install fd
+
+choco install lazygit
+
+choco install oh-my-posh
+
+choco install zoxide
+
+choco install gsudo
+
+choco install fzf
+
+choco install everything
+
+choco install msys2
+
+choco install rustup.install
+
+oh-my-posh font install meslo # Meslo LGM NF
+
+npm install -g neovim
+
+sudo cpanm -n Neovim::Ext
+
+sudo pip install --user --upgrade pynvim
+
+sudo gem install neovim
