83aae4e991971982ac5c603ef3d1e3a0963aee12
Zhang Haifeng <94585823+zhan9hf@users.noreply.github.com>
2023-10-12 23:40:14 +0800

    fix conform.nvim config bug
    formatters_by_ft with only cmake will report error when format

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -94,9 +94,15 @@ return {
 
   {
     "stevearc/conform.nvim",
-    opts = { formatters_by_ft = { cmake = "cmake_format" }, format_on_save = {
-      timeout_ms = 2000,
-    } },
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        cmake = { "cmake_format" },
+      },
+      format_on_save = {
+        timeout_ms = 2000,
+      },
+    },
   },
 
   -- {
