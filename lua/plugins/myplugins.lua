8edfc0de945d3226243b62822f8e04a5b6c5c2b3
Zhang Haifeng <94585823+zhan9hf@users.noreply.github.com>
2024-06-25 14:24:54 +0800

    change snippet

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -10,36 +10,8 @@ return {
       -- colorscheme = "onedark",
       -- colorscheme = "tokyonight",
       colorscheme = "gruvbox",
-      -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+      -- colorscheme = "seoul256",
     },
-  },
-
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- required
-      "nvim-telescope/telescope.nvim", -- optional
-      "sindrets/diffview.nvim", -- optional
-    },
-    config = true,
-    keys = {
-      {
-        "<leader>gg",
-        "<cmd>Neogit cwd=%:p:h<cr>",
-        desc = "Neogit root",
-      },
-      { "<leader>gG", "<cmd>Neogit<cr>", desc = "Neogit current" },
-    },
-  },
-
-  {
-    "s1n7ax/nvim-window-picker",
-    name = "window-picker",
-    event = "VeryLazy",
-    version = "2.*",
-    config = function()
-      require("window-picker").setup()
-    end,
   },
 
   {
@@ -49,51 +21,14 @@ return {
   },
 
   -- {
-  --   "vimwiki/vimwiki",
-  --   init = function()
-  --     vim.g.vimwiki_list = {
-  --       {
-  --         path = "~/vimwiki/",
-  --         syntax = "markdown",
-  --         ext = ".md",
-  --         index = "index",
-  --         path_html = "~/vimwiki_html/",
-  --         template_path = "~/vimwiki/templates/",
-  --         template_default = "default",
-  --         template_ext = ".md",
-  --         table_mappings = 0,
-  --       },
-  --     }
-  --     vim.g.vimwiki_global_ext = 0
+  --   "s1n7ax/nvim-window-picker",
+  --   name = "window-picker",
+  --   event = "VeryLazy",
+  --   version = "2.*",
+  --   config = function()
+  --     require("window-picker").setup()
   --   end,
-  --   keys = { {
-  --     "<leader>ww",
-  --     "<cmd>VimwikiIndex<cr>",
-  --     desc = "Vimwiki Index",
-  --   } },
   -- },
-
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
-  },
 
   {
     "stevearc/conform.nvim",
@@ -101,18 +36,6 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         cmake = { "cmake_format" },
-      },
-      -- format_on_save = {
-      --   timeout_ms = 2000,
-      -- },
-    },
-  },
-
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        cpp = { "cppcheck" },
       },
     },
   },
@@ -213,8 +136,7 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = function(_, opts)
-      -- opts.close_if_last_window = true -- Close Neo-tree if it is the last window left in the tab
-      -- opts.enable_git_status = false
+      opts.close_if_last_window = true
       opts.window = {
         width = 28,
         mappings = {
@@ -226,65 +148,14 @@ return {
     end,
   },
 
-  { "JoosepAlviste/nvim-ts-context-commentstring", enabled = false },
-  { "echasnovski/mini.comment", enabled = false },
-  {
-    "numToStr/Comment.nvim",
-    event = "VeryLazy",
-    opts = {},
-    config = function(_, opts)
-      require("Comment").setup(opts)
-    end,
-  },
-
-  { "L3MON4D3/LuaSnip", enabled = false },
-  { "rafamadriz/friendly-snippets", enabled = false },
-  { "saadparwaiz1/cmp_luasnip", enabled = false },
-  -- then: setup supertab in cmp
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-emoji",
-      "SirVer/ultisnips",
-      "quangnguyen30192/cmp-nvim-ultisnips",
-      "honza/vim-snippets",
-      "paopaol/cmp-doxygen",
     },
-    ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      opts.snippet = {
-        expand = function(args)
-          vim.fn["UltiSnips#Anon"](args.body)
-        end,
-      }
-
-      opts.preselect = "None"
-
-      local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
-      local cmp = require("cmp")
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          else
-            cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          else
-            cmp_ultisnips_mappings.jump_backwards(fallback)
-          end
-        end, { "i", "s" }),
-      })
-
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
-        { name = "cmp-emoji" },
-        { name = "ultisnips" },
-        { name = "doxygen" },
-      }))
+      -- opts.preselect = "None"
+      table.insert(opts.sources, { name = "emoji" })
     end,
   },
 }
