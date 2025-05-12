6ad6decab51914abcf745c800ea809b3fa8fdb9c
Zhang Haifeng <94585823+zhan9hf@users.noreply.github.com>
2025-05-12 10:21:43 +0800

    update

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -10,38 +10,81 @@ return {
     },
   },
 
+  -- Core package manager – last 1.x commit (May 2 2025)
+  { "williamboman/mason.nvim", commit = "4da89f3" }, -- v1.‑series
+
+  -- LSP bridge – latest 1.x release tag (Feb 15 2025)
+  { "williamboman/mason-lspconfig.nvim", commit = "1a31f82" }, -- v1.32.0
+
   {
-    "Saghen/blink.cmp",
-    version = "v0.5.1",
+    "jamespeilunli/nvim-flatbuffers",
+    event = "LspAttach",
+    config = function()
+      require("flatbuffers").setup()
+    end,
   },
 
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
-    },
-    opts = function(_, opts)
-      -- opts.preselect = "None"
-      table.insert(opts.sources, { name = "emoji" })
-      local cmp = require("cmp")
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ---@diagnostic disable-next-line: no-unknown
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ---@diagnostic disable-next-line: no-unknown
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
-    end,
+    "zchee/vim-flatbuffers",
   },
+
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = {
+  --     "hrsh7th/cmp-emoji",
+  --   },
+  --   opts = function(_, opts)
+  --     -- opts.preselect = "None"
+  --     table.insert(opts.sources, { name = "emoji" })
+  --     local cmp = require("cmp")
+  --     opts.mapping = vim.tbl_extend("force", opts.mapping, {
+  --       ---@diagnostic disable-next-line: no-unknown
+  --       ["<Tab>"] = cmp.mapping(function(fallback)
+  --         if cmp.visible() then
+  --           cmp.select_next_item()
+  --         else
+  --           fallback()
+  --         end
+  --       end, { "i", "s" }),
+  --       ---@diagnostic disable-next-line: no-unknown
+  --       ["<S-Tab>"] = cmp.mapping(function(fallback)
+  --         if cmp.visible() then
+  --           cmp.select_prev_item()
+  --         else
+  --           fallback()
+  --         end
+  --       end, { "i", "s" }),
+  --     })
+  --   end,
+  -- },
+
+  -- {
+  --   "saghen/blink.cmp",
+  --   opts = {
+  --     keymap = {
+  --       preset = "enter",
+  --       ["<C-y>"] = { "select_and_accept" },
+  --       ["<Tab>"] = {
+  --         function(cmp)
+  --           if cmp.snippet_active() then
+  --             return cmp.snippet_forward()
+  --           else
+  --             return cmp.select_next()
+  --           end
+  --         end,
+  --         "fallback",
+  --       },
+  --       ["<S-Tab>"] = {
+  --         function(cmp)
+  --           if cmp.snippet_active() then
+  --             return cmp.snippet_backward()
+  --           else
+  --             return cmp.select_prev()
+  --           end
+  --         end,
+  --         "fallback",
+  --       },
+  --     },
+  --   },
+  -- },
 }
