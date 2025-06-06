537e42d50536b0132fb296d714e83f2d00886f8e
Zhang Haifeng <94585823+zhan9hf@users.noreply.github.com>
2025-06-06 13:35:53 +0800

    temp remove lazyvim.json

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -10,12 +10,6 @@ return {
     },
   },
 
-  -- Core package manager – last 1.x commit (May 2 2025)
-  { "williamboman/mason.nvim", commit = "4da89f3" }, -- v1.‑series
-
-  -- LSP bridge – latest 1.x release tag (Feb 15 2025)
-  { "williamboman/mason-lspconfig.nvim", commit = "1a31f82" }, -- v1.32.0
-
   {
     "jamespeilunli/nvim-flatbuffers",
     event = "LspAttach",
